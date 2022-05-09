0ca0d04c1f3d883b649574a44b4cc2121e3e6599
Prakash B <prakash.b@gumstack.com>
2022-05-09 09:57:17 +0530

    added favicons

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,32 @@
 import "bootstrap/dist/css/bootstrap.css";
+import Head from "next/head";
 import "../styles/app.css";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
